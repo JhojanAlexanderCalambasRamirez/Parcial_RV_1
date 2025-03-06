b3f3b6955140a873150dff497e670916bfa4836b
JACR <98819854+JhojanAlexanderCalambasRamirez@users.noreply.github.com>
2025-03-06 14:46:14 -0500

    Postes de luz agregados

--- Parcial_1_RV/Assets/Scenes - Parcial 1/Scripts/Barrita.cs
+++ Parcial_1_RV/Assets/Scenes - Parcial 1/Scripts/Barrita.cs
@@ -2,12 +2,33 @@
 
 public class Barrita : MonoBehaviour
 {
+    public float velocidadRotacion = 50f;
+    public float amplitudMovimiento = 0.2f;
+    public float velocidadMovimiento = 2f;
+
+    private Vector3 posicionInicial;
+
+    void Start()
+    {
+        posicionInicial = transform.position;
+    }
+
+    void Update()
+    {
+        // Rotación en el eje Z (parece que gira como un trompo acostado)
+        transform.Rotate(0, 0, velocidadRotacion * Time.deltaTime);
+
+        // Movimiento arriba y abajo
+        float nuevaY = posicionInicial.y + Mathf.Sin(Time.time * velocidadMovimiento) * amplitudMovimiento;
+        transform.position = new Vector3(posicionInicial.x, nuevaY, posicionInicial.z);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("✅ Colisión con Barrita detectada. Aumentando energía y puntaje...");
-            UIManager.instancia.RecogerBarrita();  // Llamar a UIManager para actualizar puntaje
+            UIManager.instancia.RecogerBarrita();
             Destroy(gameObject);
         }
     }
